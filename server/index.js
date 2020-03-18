ae23d7172cbc64163910b80231f70c1cebea9c1a
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-03-19 00:42:28 +0300

    Request is what the user request and the response is the action...

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express();
 
 
 // Listen for the homepage
-app.use('/', (req, res));
+app.use('/', (req, res) => {
+    res.send("Hello World in NodJS");
+});
 
 
 // Run the application
